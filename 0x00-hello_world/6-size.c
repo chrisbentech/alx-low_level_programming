035d53886e2c12d7b3d045d409c208ec60e10dae
chrisbentech <chrisbenconcept458@gmail.com>
2023-02-16 10:43:38 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - A programe that prints various computer types
-*Return 0 (meaning programe is successful)
+*Return: 0 (meaning programe is successful)
 */
 int main(void)
 {
