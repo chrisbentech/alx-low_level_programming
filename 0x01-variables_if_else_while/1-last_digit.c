eef6004428e56f7eb235d4eb2c743801de5c2515
chrisbentech <chrisbenconcept458@gmail.com>
2023-02-17 08:02:24 -0800

    0-positive_or_negative

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+*main - print a text according number
+*Return: Always (Success)
+*
+*/
+
+int main(void)
+
+{
+
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
