e31ca62ecae5d3ce77562b2d75fa08169afbade1
chrisbentech <chrisbenconcept458@gmail.com>
2023-02-21 04:33:46 -0800

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
 
 void print_alphabet_x10(void)
 {
-	int ten;
-	char la;
+	int j;
+	char x;
 
-	for (ten = 0; ten <= 9; ten++)
+	for (x = 0; x < 10; x++)
 	{
-		for (la = 'a'; la <= 'z'; la++)
+		for (j = 'a'; j <= 'z'; j++)
 
 		{
-			_putchar(la);
+			_putchar(j);
 		}
 
 		_putchar('\n');
